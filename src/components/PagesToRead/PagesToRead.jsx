be3f288bef440016b129caa036242466e4943946
sagor-khan7 <sksagor791@gmail.com>
2025-07-20 00:40:02 +0600

    created basic routing and basic components

---
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PagesToRead = () => {
+  return (
+    <div>
+      <h1>
+        Display a chart with details about how many pages are left to read
+      </h1>
+    </div>
+  );
+};
+
+export default PagesToRead;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import Root from "./components/Root/Root.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
+import Home from "./components/Home/Home.jsx";
+import PagesToRead from "./components/PagesToRead/PagesToRead.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/pages-to-read",
+        element: <PagesToRead />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
