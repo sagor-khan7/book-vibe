61b060ca057ed052efafbab8c41b672eb64780b1
sagor-khan7 <sksagor791@gmail.com>
2025-07-19 23:57:14 +0600

    initial project created

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Book Vibe</h1>
+      <p>Your one-stop solution for book recommendations.</p>
+    </div>
+  );
+};
+
+export default App;
